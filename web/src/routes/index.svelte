c78b22cea5ac9a8ac8e8be4a135bd784aa6e1895
krist7599555 <krist7599555@gmail.com>
2022-03-02 05:14:53 +0700

    init

---
+++ web/src/routes/index.svelte
@@ -0,0 +1,30 @@
+<svelte:head>
+  <title>krist7599555</title>
+</svelte:head>
+
+<section class="hero is-primary is-fullheight">
+	<nav class="hero-head">
+		<nav class="navbar">
+			<div class="container">
+				<div class="navbar-brand">
+					<a class="navbar-item" href='https://github.com/krist7599555'>Github</a>
+          <span class="navbar-burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+				</div>
+			</div>
+		</nav>
+	</nav>
+	<div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">
+        Krist7599555
+      </p>
+      <p class="subtitle">
+        Krist Ponpairin
+      </p>
+    </div>
+	</div>
+</section>
